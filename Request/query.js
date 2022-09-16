588c5a3e86bf5051f5fc3811642965cfbf124e21
keshridivya <dkeshari094@gmail.com>
2022-09-16 14:40:30 +0530

    c

--- Request/query.js
+++ Request/query.js
@@ -45,7 +45,7 @@ if(val == "Full container load FCL") {
     $(".fullwagon").hide();
     $(".uldContainer").hide();
 }
-else if(val == "Full truck load FCL") {
+else if(val == "Full truck load FTL") {
     $(".fulltruck").show();
     $(".fullContain").hide();
     $(".lessContainer").hide();
@@ -53,7 +53,7 @@ else if(val == "Full truck load FCL") {
     $(".fullwagon").hide();
     $(".uldContainer").hide();
 }
-else if(val == "less truck load FCL") {
+else if(val == "less truck load LTL") {
     $(".lessContainer").show();
     $(".fulltruck").hide();
     $(".fullContain").hide();
@@ -148,81 +148,11 @@ $('.checkboxconta').hide();
         $(".after-add-more").after(html);  
     });  
 
-    $(".add").click(function () {
-        var html = '';
-        html += '<div class="row inputFormRow" id="">';
-        html += '<div class="col-md-6">';
-        html += '<div class="form-group mt-4">';
-        html += '<label class="dimlabel"><span class="dimspan">Dimensions of Box</span>';
-        html += '<div class="dimdiv">';
-        html += '<div class="weightdiv1"><span class="weightdiv1" style="max-width: initial;">';
-        html += '<input class="diminput lwhq" type="number" name="dimensions.length" placeholder="length" value="">';
-        html += '<span class="divcross"><svg width="10" height="10" fill="none" xmlns="http://www.w3.org/2000/svg">';
-        html += '<path fill-rule="evenodd" clip-rule="evenodd" d="M.529 8.529a.667.667 0 00.943.943L5 5.943l3.53 3.529a.667.667 0 10.943-.943L5.942 5l3.53-3.528a.667.667 0 00-.943-.943L5 4.057 1.472.53a.667.667 0 00-.943.943L4.057 5 .53 8.53z" fill="#6E7E92"> </path>';
-        html += '</svg></span></span><span class="weightdiv1">';
-        html += '<input class="diminput lwhq" type="number" name="dimensions.width" placeholder="width" value="">';
-        html += '<span class="divcross"><svg width="10" height="10" fill="none" xmlns="http://www.w3.org/2000/svg">';
-        html += '<path fill-rule="evenodd" clip-rule="evenodd" d="M.529 8.529a.667.667 0 00.943.943L5 5.943l3.53 3.529a.667.667 0 10.943-.943L5.942 5l3.53-3.528a.667.667 0 00-.943-.943L5 4.057 1.472.53a.667.667 0 00-.943.943L4.057 5 .53 8.53z" fill="#6E7E92"> </path>';
-        html += '</svg></span></span><span class="weightdiv1">';
-        html += '<input class="diminput lwhq" type="number" name="dimensions.height" placeholder="height" value=""></span></div>';
-        html += '<span class="mt">';
-        html += '<select name="" id="" class="dropmt"> <option value="m">m</option> <option value="cm">cm </option> </select>';
-        html += '</span>';
-        html += '</div>';
-        html += '</label>';
-        html += '</div>';
-        html += '</div>';
-        html += '<div class="col-md-2">';
-        html += '<div class="form-group mt-4">';
-        html += '<label class="label" for="email_id">Number of Box<span style="color:red">*</span></label>';
-        html += '<div class="weightdiv">';
-        html += '<div class="weightdiv1"><input class="OfQTtD lwhq" type="text" name="cargoWeight" placeholder="no of boxes"> </div>';
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-        html += '<div class="col-md-3">';
-        html += '<div class="form-group mt-4">';
-        html += '<label class="label" for="email_id">Weight of Box<span style="color:red">*</span></label>';
-        html += '<div class="weightdiv">';
-        html += '<div class="weightdiv1"><input class="OfQTtD boxweight" type="text" name="cargoWeight" placeholder="no of boxes"> </div>';
-        html += '<span class="mt">';
-        html += ' <select name="" id="" class="dropmt"> <option value="kg">kg</option> <option value="mt">mt</option></select>';
-        html += ' </span>';
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-        html += '<div class="col-md-1 ">'
-        html += '<div class="removeRow mt-5">';
-        html += '<i class="fa fa-trash"></i>';
-        html += '</div>';
-        html += '</div>';
-        html += '</div>';
-        
-        $('#newRow').append(html);
-    });
+
 
     $(document).on('click', '.removeRow', function () {
         $(this).closest('.inputFormRow').remove();
     });
-
-    // $('.lwhq').keyup(function() {
-
-    //     let length=$('.length1').val();
-    //     let width1=$('.width1').val();
-    //     let height=$('.height').val();
-    //     let quantityunit=$('.quantityunit').val();
-    //    $('.totalwh').text(length*width1*height*quantityunit);
-
-    // });
-
-    // $(document).on("keyup", ".lwhq", function() {
-    //     var mul = 0;
-    //     $(".lwhq").each(function(){
-    //         // mul += $(this).val();
-    //         mul = $(this).html(parseFloat($(this).val()) * factor);
-    //     });
-    //     $(".totalwh").text(mul);
-    // });
 
     
     $(document).on("keyup", ".boxweight", function() {
@@ -233,7 +163,53 @@ $('.checkboxconta').hide();
         $(".totalweig").text(sum);
     });
 
+    $('#weightkg').change(function(){
+        let ff=$(this).val();
+        $(".kgclass").text(ff);
+    })
+
 });
+
+
+// length
+// width
+// height
+// qty
+// $(document).on("keyup", ".lwhq", function() {
+//     var multi=1;
+//    $('.lwhq').each(function(index,item) {
+//      var val = parseFloat($(item).val());
+//      if(!isNaN(val))
+//         {
+//            multi = multi * val;
+//         }
+//    });
+//    $(".totalwh").text(multi);
+  
+//   });
+
+//   $(".length, .width, .height, .qty").keyup(function(){
+//     $('.length').each(function(index,item) {
+//         let length = parseFloat($(item).val());
+//              if(!isNaN(val))
+//                 {
+//                    multi = multi * val;
+//                 }
+//            });
+//         });
+           $('.length, .width, .height, .qty').keyup(function(){
+            let mul=0;
+            let length = parseFloat($(this).closest(".calculate").find(".length").val()) || 0;
+            let width = parseFloat($(this).closest(".calculate").find(".width").val()) || 0;
+            let height = parseFloat($(this).closest(".calculate").find(".height").val()) || 0;
+            let qty = parseFloat($(this).closest(".calculate").find(".qty").val()) || 0;
+            $(this).closest(".calculate").find(".bal").val(length * width * height * qty);  
+            $(".bal").each(function(){
+                mul += +$(this).val();
+            });
+            $(".totalwh").text(mul);  
+        });
+
 
 $(function () {
     $("#datepicker").datepicker({ 
