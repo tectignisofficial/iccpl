d4792d434e373d34d17bf4c1e194460957ad5d1e
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-08 14:15:08 +0530

    d

--- ILCCP/include/header.php
+++ ILCCP/include/header.php
@@ -37,7 +37,6 @@ $page=substr($_SERVER['SCRIPT_NAME'],strrpos($_SERVER['SCRIPT_NAME'],"/")+1);
             </ul>
           </li>
           <li><a class="nav-link <?= $page == 'blog.php' ? 'active':'' ?>" href="blog.php">Blog</a></li>
-          <!-- <li><a class="nav-link <?= $page == 'blog.php' ? 'active':'' ?>" href="blog.php">Blog</a></li> -->
           <li><a class="get-a-quote nav-link" <?= $page == 'check.php' ? 'active':'' ?>" href="check.php">Request A Quote</a></li>
           <li><a class="get-a-quote nav-link" class="nav-link <?= $page == 'book_an_appointment.php' ? 'active':'' ?>" href="book_an_appointment.php">Book An Appointment</a></li>
         </ul>
