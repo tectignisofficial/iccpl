d7aa045c27fba861a5834f40809b95c3f83771a0
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-11 14:43:53 +0530

    delete folder

--- include/header.php
+++ include/header.php
@@ -1,31 +1,46 @@
-<!-- ======= Header ======= -->
 <?php
 $page=substr($_SERVER['SCRIPT_NAME'],strrpos($_SERVER['SCRIPT_NAME'],"/")+1);
-?>   
-<header id="header" class="fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
+?> 
 
-      <div class="logo">
-        <h1><a href="index.php"><img src="assets/img/logo.png" class="img-fluid" alt=""></a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      </div>
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
+      <a href="index.php" class="logo d-flex align-items-center">
+        <img src="assets\img\logohf.webp" alt="">
+        
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto <?= $page == 'index.php' ? 'active':'' ?>" href="index.php">Home</a></li>
-          <li><a class="nav-link scrollto <?= $page == 'about.php' ? 'active':'' ?>" href="about.php">About</a></li>
-          <li><a class="nav-link scrollto <?= $page == 'services.php' ? 'active':'' ?>" href="services.php">Services</a></li>
-       
-          <li><a class="nav-link scrollto <?= $page == 'blog.php' ? 'active':'' ?>" href="blog.php">Blog</a></li>
-         
-         
-          <li><a class="getstarted scrollto <?= $page == 'check.php' ? 'active':'' ?>" href="check.php">REQUEST A QUOTE
-</a></li>
-<li><a class="getstarted scrollto <?= $page == 'contact.php' ? 'active':'' ?>" href="contact.php">Book An Appointment</a></li>
+          <li><a class="nav-link <?= $page == 'index.php' ? 'active':'' ?>" href="index.php">Home</a></li>
+          <li><a class="nav-link <?= $page == 'about.php' ? 'active':'' ?>" href="about.php">About</a></li>
+          <!-- <li><a href="services.php">Services</a></li> -->
+          <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          <ul>
+          <li><a class="nav-link <?= $page == 'supplychain.php' ? 'active':'' ?>" href="supplychain.php">Supply Chain <br>
+           Management Division</a></li>
+            <!-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li> -->
+              <li><a class="nav-link <?= $page == 'chemicaldivision.php' ? 'active':'' ?>" href="chemicaldivision.php">Chemical Division</a></li>
+              <li><a class="nav-link <?= $page == 'agrodivision.php' ? 'active':'' ?>" href="agrodivision.php">Agro Division</a></li>
+              <li><a class="nav-link <?= $page == 'project&construction.php' ? 'active':'' ?>" href="project&construction.php">Project And <br>
+                 Construction Division</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link <?= $page == 'blogs.php' ? 'active':'' ?>" href="blogs.php">Blog</a></li>
+          <li><a class="get-a-quote nav-link" <?= $page == 'check.php' ? 'active':'' ?> href="check.php">Request A Quote</a></li>
+          <li><a class="get-a-quote nav-link" class="nav-link <?= $page == 'book_an_appointment.php' ? 'active':'' ?>" href="book_an_appointment.php">Book An Appointment</a></li>
         </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
     </div>
-  </header><!-- End Header -->
+  </header>
--- sea.php
+++ sea.php
@@ -5,33 +5,35 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Sea Freight</title>
+  <title>ILCCP</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-    <link href="assets/img/logo.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/img/logo (2).webp" rel="icon">
+  <link href="assets/img/logo (2).webp" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-  <link href="assets/css/footer.css" rel="stylesheet">
+  <link href="assets/css/main.css" rel="stylesheet">
 
   <!-- =======================================================
-  * Template Name: Vesperr - v4.8.1
-  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Template Name: Logis - v1.2.0
+  * Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
@@ -40,126 +42,140 @@
 <body>
 
   <!-- ======= Header ======= -->
-  <?php include"include/header.php";?>
+  <?php include("include/header.php");?>
+  <!-- End Header -->
   <!-- End Header -->
 
   <main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container">
-
-        
-      </div>
-    </section><!-- End Breadcrumbs -->
-
-    <!-- ======= Portfolio Details Section ======= -->
-    <section id="portfolio-details" class="portfolio-details">
-      <div class="container">
-      <div class="section-title" data-aos="fade-up">
-            <h2>SEA FREIGHT</h2>
-          </div>
-        <div class="row gy-4">
-
-          <div class="col-lg-12">
-            <div class="portfolio-details-slider swiper">
-              <div class="swiper-wrapper align-items-center">
-
-               
-
-                
-                <div class="" data-aos="fade-up">
-                  <img src="assets/img/team/ship2.jpg" alt="" style="margin-left: 14%;">
-                </div>
-
-              </div>
-              <div class="swiper-pagination"></div>
+    <div class="breadcrumbs">
+      <div class="page-header d-flex align-items-center" style="background-image: url('assets/img/page-header.webp');">
+        <div class="container position-relative">
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-6 text-center">
+              <h2>Sea Freight</h2>
             </div>
           </div>
-         
-
-          <div class="row content">
-            
-            <div class="col-lg-12 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="300">
-            <p  style="margin-left: 6%;     margin-top: 41px;
-">
-We bring your goods safely to worldwide destinations with our great sea fright services. We offer LLC and FLC shipments that are fast and effective with no delays.
-              </p>
-            <p  style="margin-left: 6%;     margin-top: 41px;
-">
-At ICCPL we deliver highly flexible services through our partnerships with leading shipping lines.The cultivation of long-term, secure partnerships with major shipping lines brings tangible benefits to our customers as we have reliable links with all commercial centres worldwide which is why our Sea Freight services are among the best in the marketplace.
-
-              </p>
-              <p style="margin-left: 6%;     margin-top: 41px;
-">Through these connections we can guarantee capacity and also offer adaptability to the ever changing shipping regulations of every country and port thus enabling a smooth passage for your consignment.</p>
-              
-            
-            </div>
-           
-          </div>
-  
-
-          
-
         </div>
-
       </div>
-    </section><!-- End Portfolio Details Section -->
-    
+
+    </div><!-- End Breadcrumbs -->
+
     <section id="about" class="about">
-        <div class="container">
-  
-         
-  
-          <div class="row content">
-            
-            <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="300">
-              <h4><b>Your cargo is safe with us</b></h4>
-              <p>
-              A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.
-
-              </p>
-              <p>After more than 40 years in business as a global transportation provider, our consultative approach coupled with a vast worldwide network has forged trusted partnerships across nearly every industry, including aerospace, e-commerce, food, government, healthcare, life sciences, retail and more.</p>
-            
+      <div class="container" data-aos="fade-up">
+        <div class="row gy-4">
+          <div class="col-lg-6  content position-relative align-self-start order-lg-last order-first mt-5">
+            <h3>About Sea Freight</h3>
+            <p>We bring your goods safely to worldwide destinations with our great sea fright services. We offer LCL and FLC shipments that are fast and effective with no delays.</p>
+            <p>At ILCCPL we deliver highly flexible services through our partnerships with leading shipping lines.The cultivation of long-term, secure partnerships with major shipping lines brings tangible benefits to our customers as we have reliable links with all commercial centres worldwide which is why our Sea Freight services are among the best in the marketplace.</p>
+            <p>Through these connections we can guarantee capacity and also offer adaptability to the ever changing shipping regulations of every country and port thus enabling a smooth passage for your consignment.</p>
             </div>
-            <div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
-            <h4><b>What makes us the best choice</b></h4>
-              <p>
-              Our network participation is with trusted international counterparts on the other end of the shipment who are equally dedicated to serve your interests. Our cooperating agents take the same care with your shipment as your customer or vendor would.
-              </p>
-              <p>
-              All of our employees are experienced in all aspects of the freight forwarding process including import and export documentation requirements for all types of cargo.
-              </p>
-              <ul>
-                <li><i class="ri-check-double-line"></i>Providing high quality transportation services to all of our clients</li>
-                <li><i class="ri-check-double-line"></i>Invest in our employees to provide better service and company growth</li>
-                <li><i class="ri-check-double-line"></i> Worry about enviroment according to latest industry standards
-</li>
-                <li><i class="ri-check-double-line"></i> Safety as top priority in assuring safe work procedures</li>
-                <li><i class="ri-check-double-line"></i> Investing in technology to provide fast, acurate and cost-effective service</li>
-                <li><i class="ri-check-double-line"></i> Living up to highest industry standards</li>
-              </ul>
-            </div>
-          </div>
-  
+        <div class="col-lg-6 order-first  order-lg-last pb-5">
+        <img src="assets\img\ship2.webp" alt="" class="img-fluid">
+           </div>
         </div>
-      </section>
+
+      </div>
+    </section>
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row gy-4">
+          <div class="col-lg-6 content position-relative align-self-start order-lg-first order-first">
+         <h3>Our Sea Freight service includes :</h3>
+         <ul>
+              <li>
+                <i class="fa fa-check-double"></i>
+                <!-- ODC Heavy Machinery, Project Cargo by Mechanical trailers / Hydraulic Axles -->
+                Ocean Freight LCL Consolidation
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+               FCL Movement of 20 GP / 40 GP / Open Top / Flat Rack Container
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                <!-- 7.5MT / 9MT / 16MT vehicles -->
+                Break- Bulk and Cargo Handling
+              </li>
+              <li style="margin-top:10px">
+
+                <i class="fa-solid fa-check-double"></i>
+                <!-- Break Bulk Cargo Movement -->
+                ODC Cargo Handling 
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                <!-- Containerized Vehicles of 20 ft/24 ft / 32 ft length -->
+                ISO Tank Container Handling
+              </li>
+              <!-- <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                Cranes & Forklifts Available
+              </li> -->
+            </ul>
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-last">
+                <h3>Some of our capabilities include:</h3>
+                <p>With our expertise in road and rail logistics operations,we can offer optimum,reasonable,coordinated and competent transport solutions.</p>
+                <ul>
+              <li>
+                <i class="fa fa-check-double"></i>
+                Transportation of heavy and odd dimensional cargo to any destination in India.
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                Project cargo movement from inception to erection and commission.
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                Delivery to consignee destination of import cargo after custom clearance from aircargo and seaport on our own delivery trucks.
+              </li>
+              <li style="margin-top:10px">
+
+                <i class="fa-solid fa-check-double"></i>
+                Personal baggage trucking to passenger doorstep after clearance.
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                    Container transportation trailers for all inland destinations in India.
+              </li>
+              <li style="margin-top:10px">
+                <i class="fa-solid fa-check-double"></i>
+                Pickup of Export Cargo from shipper warehouse to nearest port.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-md-12 text-center">
+           <a href="check.php"><button class="btn btn-primary">Request A Quote</button></a>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Us Section -->
 
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-    <?php include"include/f.php";?>
+  <?php include("include/footer.php");?>
+  <!-- End Footer -->
   <!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
