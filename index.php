b8d37a58240c53b277d132e98bf0bc8c75f9bde9
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-15 12:14:06 +0530

    index

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html lang="en">
 
 <head>
+ 
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Index</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <title>INTRACONTINENT LOGICHEM CORPORATION PRIVATE LIMITED</title>
+  <meta content="INTRACONTINENT LOGICHEM CORPORATION PRIVATE LIMITED" name="description">
+  <meta content="INTRACONTINENT LOGICHEM CORPORATION PRIVATE LIMITED, iccpl, iccpl" name="keywords">
 
   <!-- Favicons -->
   <link href="assets1/img/logo (2).webp" rel="icon">
