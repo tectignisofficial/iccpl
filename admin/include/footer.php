10132d96fde2c98601f2c340b148e379d4f1048d
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-20 17:41:09 +0530

    changes

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
      <b>Developed by<a href="http://tectignis.in" target="_blank" style="color:#869099">Tectignis IT Solutions</a> </b>
     </div>
-    <strong>&copy; 2022 <a href="http://tectignis.in" target="_blank" style="color:#869099">ILCCP</a>.</strong> All rights reserved.
+    <strong>&copy; 2022 <a href="https://iccpl.tectignis.in" target="_blank" style="color:#869099">ILCCP</a>.</strong> All rights reserved.
   </footer>
