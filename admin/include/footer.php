90d798037ff65944cc510b37cfcdcd488c59177a
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-20 17:39:34 +0530

    color

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-     <b>Developed by<a href="http://tectignis.in" target="_blank">Tectignis IT Solutions</a> </b>
+     <b>Developed by<a href="http://tectignis.in" target="_blank" style="color:#869099">Tectignis IT Solutions</a> </b>
     </div>
-    <strong>&copy; 2022 <a href="http://tectignis.in" target="_blank">ILCCP</a>.</strong> All rights reserved.
+    <strong>&copy; 2022 <a href="http://tectignis.in" target="_blank" style="color:#869099">ILCCP</a>.</strong> All rights reserved.
   </footer>
