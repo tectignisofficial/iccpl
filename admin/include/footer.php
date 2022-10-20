479572594ec5af6805702ce3c02226aa0236a143
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-20 17:38:04 +0530

    footer link

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+     <b>Developed by<a href="http://tectignis.in" target="_blank">Tectignis IT Solutions</a> </b>
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>&copy; 2022 <a href="http://tectignis.in" target="_blank">ILCCP</a>.</strong> All rights reserved.
   </footer>
